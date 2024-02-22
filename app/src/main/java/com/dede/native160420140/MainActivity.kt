d2e2bd9deec5d023051db20554da744166558ab5
DedeJP <s160420140@student.ubaya.ac.id>
2024-02-22 14:07:58 +0700

    init

---
+++ app/src/main/java/com/dede/native160420140/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dede.native160420140
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
